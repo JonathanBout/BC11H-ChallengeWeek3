28c07e671929cb30a81549f9d926308a8090cfad
RFlintstone <rubenflint@hotmail.com>
2023-11-27 16:35:45 +0100

    It's (partially) rotating. Up to keydown events.

--- game.py
+++ game.py
@@ -10,32 +10,79 @@ class Game:
     def __init__(self):
         # initialize pygame
         pygame.init()
+
+        # initialize screen and clock
         self.screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
+        self.frame_counter = 0  # add counter for frames
+
+        # Initialize frame_update_interval using config variable
+        self.frame_update_interval = c.PLAYER_FRAME_UPDATE_INTERVAL
 
         # initialize game objects
         self.world = World(c, c.WORLD_NAME, c.WORLD_DESCRIPTION, c.WORLD_POSITION)
         self.race_track = Map(c, c.MAP_NAME, c.MAP_DESCRIPTION, c.MAP_POSITION)
         self.player = Player(c, c.PLAYER_NAME, c.PLAYER_DESCRIPTION, c.PLAYER_POSITION)
 
-    def start(self):
-        # Set background color
-        self.screen.fill("purple")
+        # Player properties
+        self.flip_player = None
+        self.player1 = self.player.prepare(c.PLAYER_CURRENT_FRAME)
 
+    def start(self):
         # Start game loop
         self.update()
 
     def update(self):
+        # Refresh the screen
+        self.refresh_screen()
+
+        num_frames = 10  # total number of frames in the animation
+
         while True:
             # traversing through every event
             for event in pygame.event.get():
                 # if the event type is QUIT then exit the program
                 if event.type == pygame.QUIT:
                     exit()
+
+            # Calculate the current frame
+
+            #         # Keydown events
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_UP:
+            #         print("Up key pressed.")
+            #     elif event.key == pygame.K_DOWN:
+            #         print("Down key pressed.")
+            #     elif event.key == pygame.K_LEFT:
+            #         print("Left key pressed.")
+            #         self.flip_player = True
+            #     elif event.key == pygame.K_RIGHT:
+            #         print("Right key pressed.")
+            #         self.flip_player = False
+            #     self.flip_player = not self.flip_player
+
+            current_frame = (self.frame_counter // self.frame_update_interval) % num_frames
+            c.PLAYER_CURRENT_FRAME = current_frame
+
+            self.player1 = self.player.prepare(c.PLAYER_CURRENT_FRAME)
+
+            # Check if the player image should be flipped
+            if self.flip_player:
+                self.player1 = pygame.transform.flip(self.player1, True,
+                                                     False)  # Flip the image horizontally, not vertically
+
+            # Update the frame counter
+            self.frame_counter += 1
+
+            print(f"Current frame: {current_frame}")
+
             # render everything
             self.render()
 
     def render(self):
+        # Draw the player1 after refreshing the screen
+        self.screen.blit(self.player1, (self.player.position[0], self.player.position[1]))
+
         # Update the full display surface to the screen
         pygame.display.flip()
 
@@ -45,9 +92,23 @@ class Game:
         # printing the frames per second (fps) rate
         c.CURRENT_FPS = self.clock.get_fps()
 
-        print(c.CURRENT_FPS)
+        # print("FPS:", c.CURRENT_FPS)
+
+    def refresh_screen(self):
+        # Clear the screen by filling it with a single color (black in this case)
+        self.screen.fill((0, 0, 0))
+
+        # Fill the screen with a custom background
+        background = pygame.image.load(c.SCREEN_BACKGROUND)
+        background = pygame.transform.scale(background, c.SCREEN_SIZE)
+        self.screen.blit(background, (0, 0))
 
     def print_config(self):
         self.world.print_config()
         self.race_track.print_config()
         self.player.print_config()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.start()
--- player.py
+++ player.py
@@ -1,9 +1,36 @@
+import pygame
+
+import config
 from world import World
 
 
 class Player(World):
     def __init__(self, config, name, description, position):
         super().__init__(config, name, description, position)
+
+    def prepare(self, frame=0):
+        sprite_path = self.config.PLAYER_SPRITE
+        game_image = pygame.image.load(sprite_path).convert_alpha()
+
+        sprite_width = self.config.PLAYER_SPRITE_WIDTH
+        sprite_height = self.config.PLAYER_SPRITE_HEIGHT
+
+        num_sprites = game_image.get_width() // sprite_width
+
+        # List to store all sprites
+        sprites = []
+        for i in range(num_sprites):
+            sprite_x = i * sprite_width
+            sprite = game_image.subsurface(pygame.Rect((sprite_x, 0, sprite_width, sprite_height)))
+            sprite = pygame.transform.scale(sprite, (sprite_width * config.PLAYER_SPRITE_SCALE, sprite_height * config.PLAYER_SPRITE_SCALE))
+            sprites.append(sprite)
+
+        # print("Player prepared")
+        return sprites[frame]
+
+    def move(self, frame=0):
+        pass
+
 
     def print_config(self):
         if self.config is None:
