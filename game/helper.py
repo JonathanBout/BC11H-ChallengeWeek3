8279e506c33cee63c5763a66afd7f2f26f49bcaf
Jonathan Bout <jonathanbout01@gmail.com>
2023-11-30 16:22:19 +0100

    always show the sad face when exiting

--- game/helper.py
+++ game/helper.py
@@ -6,9 +6,8 @@ def exit_if_user_wants():
     Helper to close the window whenever the X is clicked.
     """
     if len(pygame.event.get(eventtype=pygame.QUIT)) > 0:
-        why_quit_if_you_can_stay()
         exit()
 
 
 def why_quit_if_you_can_stay():
-    print("😭why are you going away?")
+    print("😭 why are you going away?")
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from game import Game
+from game import Game, helper
+import atexit
 
 
 def main():
+    atexit.register(__at_exit)
     game = Game()  # Initialize game
     game.print_config()  # Print game config
     game.start()  # Start the game
+
+
+def __at_exit():
+    helper.why_quit_if_you_can_stay()
 
 
 if __name__ == "__main__":
