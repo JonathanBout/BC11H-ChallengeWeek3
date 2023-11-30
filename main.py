b78026d3a6a29ee64f1138e7dc4ca8f987282625
Jonathan Bout <jonathanbout01@gmail.com>
2023-11-30 18:01:11 +0100

    no windows api :(

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 from game import Game, helper
 import atexit
-import win32gui
-import win32con
 
 
 def main():
-    # hide terminal
-    win32gui.ShowWindow(win32gui.GetForegroundWindow(), win32con.SW_HIDE)
-
     atexit.register(__at_exit)
     game = Game()  # Initialize game
     game.print_config()  # Print game config
