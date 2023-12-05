467a906cd75dbb61e591b12bc2cc6346e3bf937a
RFlintstone <rubenflint@hotmail.com>
2023-12-05 22:34:45 +0100

    Why isn't this working?!

--- game/powerup.py
+++ game/powerup.py
@@ -3,14 +3,14 @@ import random
 
 from game import config
 
-POWERUP_RESPAWN_EVENT = pygame.USEREVENT + 1  # Event for when the powerup respawns
-
 
 class Powerup(pygame.sprite.Sprite):
+
     def __init__(self, id):
         super().__init__()
 
         # Keep track of the amount of powerups
+        self.POWERUP_RESPAWN_EVENT = pygame.USEREVENT + 1  # Event for when the powerup respawns
         self.amount_powerups = id  # Give each powerup a unique id
         self.powerups = None  # Which powerups are on the map
         self.powerup_respawn_list = []  # Which powerups should be respawned
@@ -20,7 +20,7 @@ class Powerup(pygame.sprite.Sprite):
         self.POWERUP_RESPAWN_TIME = 5000  # When the powerup respawns
         self.powerup_respawn_time = None  # How long the powerup has been gone
         self.current_time = None  # The current time
-        pygame.time.set_timer(POWERUP_RESPAWN_EVENT, self.POWERUP_RESPAWN_TIME)
+        pygame.time.set_timer(self.POWERUP_RESPAWN_EVENT, self.POWERUP_RESPAWN_TIME)
 
         # Load the sprite
         self.image = pygame.image.load("assets/sprites/powerup_box.png").convert_alpha()  # Load the sprite
@@ -46,10 +46,12 @@ class Powerup(pygame.sprite.Sprite):
         self.powerups = powerups
 
         for event in pygame.event.get(
-                POWERUP_RESPAWN_EVENT
+                self.POWERUP_RESPAWN_EVENT
         ):
-            if event.type == POWERUP_RESPAWN_EVENT:
+            if event.type == self.POWERUP_RESPAWN_EVENT:
+                print("Powerup respawned - TRIGGERED")
                 self.respawn_powerups()
+                pygame.display.flip()
 
         for powerup in self.powerups:
             if player_rect.colliderect(powerup):
@@ -62,8 +64,10 @@ class Powerup(pygame.sprite.Sprite):
             self.powerups.add(p)
         self.powerup_respawn_list.clear()
 
-
     def on_pick_up(self, powerup):
+        event = pygame.event.Event(self.POWERUP_RESPAWN_EVENT)
+        pygame.event.post(event)
+
         self.powerups.remove(powerup)
         self.picked_up_item.append(random.choice(self.effects))
         self.powerup_respawn_list.append(powerup)
